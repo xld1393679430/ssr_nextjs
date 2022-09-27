c1eb021dd2e3d860adcfd5c8cd1e2b058c17ad73
ld_xu <ld_xu@trip.com>
2022-09-27 15:53:15 +0800

    搭建一个 Nextjs 服务器端渲染项目

---
+++ pages/Home/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.scss'
+
+const Index = () => {
+    return (
+        <div className={styles.container}>
+            <p className={styles.text}>Home</p>
+        </div>
+    )
+}
+
+export default Index
